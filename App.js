f38eb5660b615712ebdbc8f3d8a5768a5d628069
Ario Tamadoni <ariozone6@gmail.com>
2020-07-25 19:52:27 -0700

    Implement basic alert with buttons

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, SafeAreaView, Alert } from "react-native"
+import { Button } from "react-native"
 
 export default function App() {
+  const handlePress = () => {
+    Alert.alert("Message:", "Do you want to continiue?", [
+      { text: "Yes", onPress: () => console.log("Yes") },
+      { text: "No", onPress: () => console.log("No") },
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <Button title='Click Me' onPress={handlePress} />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
