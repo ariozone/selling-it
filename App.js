5bb75b8789b657343ae5732b26ddbb1d08c5e70b
Ario Tamadoni <ariozone6@gmail.com>
2021-07-07 20:04:51 -0700

    Settings modified

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import { StyleSheet, SafeAreaView, Alert } from "react-native"
 import { Button } from "react-native"
 
 export default function App() {
+  console.log('App executed')
   const handlePress = () => {
     Alert.alert("Message:", "Do you want to continiue?", [
       { text: "Yes", onPress: () => console.log("Yes") },
       // { text: "Maybe", onPress: () => console.log("Maybe") },
       { text: "No", onPress: () => console.log("No") },
     ])
+    // Alert.prompt("Message:", "Enter message here:", (text) => console.log(text))
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button title='Click Me' onPress={handlePress} />
+      <Button title='Click Me!' onPress={handlePress} />
     </SafeAreaView>
   )
 }
