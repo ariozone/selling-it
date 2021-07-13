ef8bf7a04432421617ee8161be24a68b0ce9822d
Ario Tamadoni <ariozone6@gmail.com>
2021-07-13 15:42:35 -0700

    Applied styles to the app.

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import { StatusBar } from "expo-status-bar"
+
 import React from "react"
-import { StyleSheet, SafeAreaView, Alert } from "react-native"
+import { Dimensions } from "react-native"
+import { View } from "react-native"
+import { StyleSheet, SafeAreaView, Alert, Platform, StatusBar } from "react-native"
 import { Button } from "react-native"
 
 export default function App() {
-  console.log('App executed')
-  const handlePress = () => {
-    Alert.alert("Message:", "Do you want to continiue?", [
-      { text: "Yes", onPress: () => console.log("Yes") },
-      // { text: "Maybe", onPress: () => console.log("Maybe") },
-      { text: "No", onPress: () => console.log("No") },
-    ])
-    // Alert.prompt("Message:", "Enter message here:", (text) => console.log(text))
-  }
+  console.log(Dimensions.get('screen'))
+  // const handlePress = () => {
+  //   Alert.alert("Message:", "Do you want to continiue?", [
+  //     { text: "Yes", onPress: () => console.log("Yes") },
+  //     // { text: "Maybe", onPress: () => console.log("Maybe") },
+  //     { text: "No", onPress: () => console.log("No") },
+  //   ])
+  //   // Alert.prompt("Message:", "Enter message here:", (text) => console.log(text))
+  // }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button title='Click Me!' onPress={handlePress} />
+      {/* <Button title='Click Me!' onPress={handlePress} /> */}
+      <View style={{ backgroundColor: 'dodgerblue', width: '50%', height: 70 }}></View>
     </SafeAreaView>
   )
 }
@@ -25,7 +28,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 })
