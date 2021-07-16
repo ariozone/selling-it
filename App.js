958dafaa7c141246099bcc047987b2ad646ce62f
Ario Tamadoni <ariozone6@gmail.com>
2021-07-15 18:08:29 -0700

    Style components created.

--- App.js
+++ App.js
@@ -8,8 +8,42 @@ import WelcomeScreen from "./app/screens/WelcomeScreen"
 export default function App() {
   return (
     // <WelcomeScreen>
-
     // </WelcomeScreen>
-    <ViewImageScreen></ViewImageScreen>
+    // <ViewImageScreen></ViewImageScreen>
+
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100,
+        shadowColor: 'gray',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 30
+
+
+      }}>
+        <View style={{
+          backgroundColor: 'gold',
+          width: 50,
+          height: 50
+        }}></View>
+      </View>
+      <View style={{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100,
+        marginTop: 30
+      }}></View>
+
+    </View>
   )
 }
