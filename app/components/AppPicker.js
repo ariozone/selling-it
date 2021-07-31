b4b85374639835c51bb11d359eda2183857cc619
Ario Tamadoni <ariozone6@gmail.com>
2021-07-30 17:00:07 -0700

    Create modal component

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Modal, StyleSheet, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import Screen from './Screen';
 import defaultStyles from '../config/styles';
 import AppText from './AppText';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 function AppPicker({ icon, placeholder, ...otherProps }) {
+  const [modalVisible, setModalVisible] = useState(false)
   return (
-    <View style={styles.container}>
-      {icon && <MaterialCommunityIcons name={icon} size={25} color={defaultStyles.colors.medium} style={styles.icon} />}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons name='chevron-down' size={25} color={defaultStyles.colors.medium} />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && <MaterialCommunityIcons name={icon} size={25} color={defaultStyles.colors.medium} style={styles.icon} />}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons name='chevron-down' size={25} color={defaultStyles.colors.medium} />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType='slide'>
+        <Screen>
+
+          <Button title='close' onPress={() => setModalVisible(false)}></Button>
+        </Screen>
+      </Modal>
+    </>
   );
 }
 
@@ -29,7 +42,10 @@ const styles = StyleSheet.create({
   icon: {
     marginRight: 10
   },
-  text: { flex: 1 }
+  text: { flex: 1 },
+  modal: {
+    backgroundColor: 'blue'
+  }
 })
 
 export default AppPicker;
