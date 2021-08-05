f2aead637d9b86f2c722bfcded7d54f9cb312b6d
Ario Tamadoni <ariozone6@gmail.com>
2021-08-04 17:34:36 -0700

    Refactor form component to a reusable module.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
-import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
-import { Formik } from 'formik'
 import * as Yup from 'yup'
 import AppFormField from '../components/AppFormField';
 import SubmitButton from '../components/SubmitButton';
+import AppForm from '../components/AppForm';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().trim().min(5).label('Email'),
@@ -17,37 +16,31 @@ function LoginScreen(props) {
   return (
     <Screen style={styles.container}>
       <Image source={require('../assets/logo-red.png')} style={styles.logo}></Image>
-      <Formik
+      <AppForm
         initialValues={{ email: '', password: '' }}
         onSubmit={values => console.log(values)}
         validationSchema={validationSchema}
       >
-        {() => (
-          <>
-
-            <AppFormField
-              name='email'
-              icon='email'
-              placeholder='Email'
-              autoCapitalize='none'
-              autoCorrect={false}
-              keyboardType='email-address'
-              textContentType='emailAddress'>
-            </AppFormField>
-            <AppFormField
-              name='password'
-              icon='lock'
-              placeholder='Password'
-              autoCapitalize='none'
-              autoCorrect={false}
-              textContentType='password'
-              secureTextEntry
-            ></AppFormField>
-            <SubmitButton title='login'></SubmitButton>
-            {/* <AppButton title='Login' onPress={handleSubmit} /> */}
-          </>
-        )}
-      </Formik>
+        <AppFormField
+          name='email'
+          icon='email'
+          placeholder='Email'
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='email-address'
+          textContentType='emailAddress'>
+        </AppFormField>
+        <AppFormField
+          name='password'
+          icon='lock'
+          placeholder='Password'
+          autoCapitalize='none'
+          autoCorrect={false}
+          textContentType='password'
+          secureTextEntry
+        ></AppFormField>
+        <SubmitButton title='login'></SubmitButton>
+      </AppForm>
 
     </Screen>
   );
