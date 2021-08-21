a016f04f6b18c334d01068560cacee9d3dd90dc0
Ario Tamadoni <ariozone6@gmail.com>
2021-08-20 17:44:34 -0700

    Enhance image input list to scroll horizontally.

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import ImageInput from './ImageInput';
 
 function ImageInputList({ imageUris = [], onAddImage, onRemoveImage }) {
+  const scrollView = useRef()
   return (
-    <View style={styles.container}>
-      {imageUris.map(imageUri => (
-        <View key={imageUri} style={styles.image}>
-          <ImageInput
+    <View>
+      <ScrollView
+        horizontal
+        ref={scrollView}
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map(imageUri => (
+            <View key={imageUri} style={styles.image}>
+              <ImageInput
 
-            imageUri={imageUri}
-            onChangeImage={() => { onRemoveImage(imageUri) }}
-          ></ImageInput>
-        </View>))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)}></ImageInput>
+                imageUri={imageUri}
+                onChangeImage={() => { onRemoveImage(imageUri) }}
+              ></ImageInput>
+            </View>))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)}></ImageInput>
+        </View>
+      </ScrollView>
     </View>
+
+
   );
 }
 
