7912196176335937729a3c64b72318db387a8e00
Ario Tamadoni <ariozone6@gmail.com>
2021-08-24 19:05:19 -0700

    Create FormImagePicker component

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import ImageInputList from '../ImageInputList';
+import ErrorMessage from './ErrorMessage';
+import { useFormikContext } from 'formik'
+
+function FormImagePicker({ name }) {
+  const { errors, touched, setFieldValue, values } = useFormikContext()
+  const imageUris = values[name]
+  const handleAdd = uri => { setFieldValue(name, [...imageUris, uri]) }
+  const handleDelete = uri => { setFieldValue(name, imageUris.filter(u => u !== uri)) }
+
+  return (
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleDelete}
+      ></ImageInputList>
+      <ErrorMessage></ErrorMessage>
+    </>
+  );
+}
+
+
+
+export default FormImagePicker;
